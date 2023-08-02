f3e7f011b0f0039fcf4658fc2bdbd1e17bf6cd28
berkecaliskan <berkecaliskan2001@gmail.com>
2023-08-02 15:13:01 +0900

    all features, area norm.

--- feature_extraction.py
+++ feature_extraction.py
@@ -58,6 +58,178 @@ def cart2sph(x, y, z):
 
    return r, theta, phi
 
+def first_time_slot(data_points):
+    if (len(data_points) == 0):
+        n_hit = 0
+        t_max = t_min = t_avg = t_var = t_std = x_avg = y_avg = z_avg = -1
+        return n_hit, t_max, t_min, t_avg, t_var, t_std, x_avg, y_avg, z_avg
+    else:
+        first_slot_datapoints = []
+        for data_point in data_points:
+            if (data_point.time <= 166573):
+                first_slot_datapoints.append(data_point)
+        data_points = first_slot_datapoints
+        if len(data_points) == 0:
+            n_hit = 0
+            t_max = t_min = t_avg = t_var = t_std = x_avg = y_avg = z_avg = -1
+            return n_hit, t_max, t_min, t_avg, t_var, t_std, x_avg, y_avg, z_avg
+        else:
+            n_hit = len(data_points)
+            if n_hit == 1:
+                t_max = data_points[0].time*1e-6
+                t_min = data_points[0].time*1e-6
+                
+                t_avg = data_points[0].time*1e-6
+                t_var = 0
+                t_std = 0
+
+                x_avg = data_points[0].x
+                y_avg = data_points[0].y
+                z_avg = data_points[0].z
+            else:
+                t_max = 0
+                t_min = np.inf
+                all_times = []
+                all_x_coordinates = []
+                all_y_coordinates = []
+                all_z_coordinates = []
+                for data_point in data_points:
+                    time = data_point.time*1e-6
+                    if time > t_max:
+                        t_max = time
+                    if time < t_min:
+                        t_min = time
+                    all_times.append(time)
+                    all_x_coordinates.append(data_point.x)
+                    all_y_coordinates.append(data_point.y)
+                    all_z_coordinates.append(data_point.z)
+                
+                t_avg = np.mean(all_times)
+                t_var = np.var(all_times)
+                t_std = np.std(all_times)
+
+                x_avg = np.mean(all_x_coordinates)
+                y_avg = np.mean(all_y_coordinates)
+                z_avg = np.mean(all_z_coordinates)
+
+        return n_hit, t_max, t_min, t_avg, t_var, t_std, x_avg, y_avg, z_avg
+
+def second_time_slot(data_points):
+    if (len(data_points) == 0):
+        n_hit = 0
+        t_max = t_min = t_avg = t_var = t_std = x_avg = y_avg = z_avg = -1
+        return n_hit, t_max, t_min, t_avg, t_var, t_std, x_avg, y_avg, z_avg
+    else:
+        second_slot_datapoints = []
+        for data_point in data_points:
+            if (data_point.time <= 333147):
+                second_slot_datapoints.append(data_point)
+        data_points = second_slot_datapoints
+        if(len(data_points) == 0):
+            n_hit = 0
+            t_max = t_min = t_avg = t_var = t_std = x_avg = y_avg = z_avg = -1
+            return n_hit, t_max, t_min, t_avg, t_var, t_std, x_avg, y_avg, z_avg
+        else:
+            n_hit = len(data_points)
+            if n_hit == 1:
+                t_max = data_points[0].time*1e-6
+                t_min = data_points[0].time*1e-6
+                
+                t_avg = data_points[0].time*1e-6
+                t_var = 0
+                t_std = 0
+
+                x_avg = data_points[0].x
+                y_avg = data_points[0].y
+                z_avg = data_points[0].z
+            else:
+                t_max = 0
+                t_min = np.inf
+                all_times = []
+                all_x_coordinates = []
+                all_y_coordinates = []
+                all_z_coordinates = []
+                for data_point in data_points:
+                    time = data_point.time*1e-6
+                    if time > t_max:
+                        t_max = time
+                    if time < t_min:
+                        t_min = time
+                    all_times.append(time)
+                    all_x_coordinates.append(data_point.x)
+                    all_y_coordinates.append(data_point.y)
+                    all_z_coordinates.append(data_point.z)
+                
+                t_avg = np.mean(all_times)
+                t_var = np.var(all_times)
+                t_std = np.std(all_times)
+
+                x_avg = np.mean(all_x_coordinates)
+                y_avg = np.mean(all_y_coordinates)
+                z_avg = np.mean(all_z_coordinates)
+
+        return n_hit, t_max, t_min, t_avg, t_var, t_std, x_avg, y_avg, z_avg
+
+def third_time_slot(data_points):
+    if (len(data_points) == 0):
+        n_hit = 0
+        t_max = t_min = t_avg = t_var = t_std = x_avg = y_avg = z_avg = -1
+        return n_hit, t_max, t_min, t_avg, t_var, t_std, x_avg, y_avg, z_avg
+    else:
+        n_hit = len(data_points)
+        if n_hit == 1:
+            t_max = data_points[0].time*1e-6
+            t_min = data_points[0].time*1e-6
+            
+            t_avg = data_points[0].time*1e-6
+            t_var = 0
+            t_std = 0
+
+            x_avg = data_points[0].x
+            y_avg = data_points[0].y
+            z_avg = data_points[0].z
+        else:
+            t_max = 0
+            t_min = np.inf
+            all_times = []
+            all_x_coordinates = []
+            all_y_coordinates = []
+            all_z_coordinates = []
+            for data_point in data_points:
+                time = data_point.time*1e-6
+                if time > t_max:
+                    t_max = time
+                if time < t_min:
+                    t_min = time
+                all_times.append(time)
+                all_x_coordinates.append(data_point.x)
+                all_y_coordinates.append(data_point.y)
+                all_z_coordinates.append(data_point.z)
+            
+            t_avg = np.mean(all_times)
+            t_var = np.var(all_times)
+            t_std = np.std(all_times)
+
+            x_avg = np.mean(all_x_coordinates)
+            y_avg = np.mean(all_y_coordinates)
+            z_avg = np.mean(all_z_coordinates)
+
+        return n_hit, t_max, t_min, t_avg, t_var, t_std, x_avg, y_avg, z_avg
+
+def normalize_area(nodes):
+    print(nodes)
+    nodes = np.array(nodes)
+    area_mean = np.mean(nodes[:, 33], axis=0)
+    area_std = np.std(nodes[:, 33], axis=0)
+    for node in nodes:
+        area = node[33]
+        
+        # Calculate the Z-score normalized values for the 36th column
+        normalized_area = (area - area_mean) / (area_std*1.0)
+        
+        # Replace the 36th column with the normalized values
+        node[33] = normalized_area
+    return nodes
 
 current_directory = os.getcwd()
 sub_directory = 'test/2tx/results'
@@ -65,13 +237,13 @@ directory_path = os.path.join(current_directory, sub_directory)
 
 
 # create the directory for storing data, if it is not created
-if not os.path.exists("gnn_data"):
-    os.makedirs("gnn_data")
+if not os.path.exists("gnn_data_son"):
+    os.makedirs("gnn_data_son")
 
 results_list = os.listdir(directory_path)
 for i in range(len(results_list)):
     nodes = []
-    count = 0
+
     file_path = os.path.join(directory_path, results_list[i])
     
     match = re.search(r"_\d+(\.)", results_list[i])
@@ -85,28 +257,6 @@ for i in range(len(results_list)):
     time = data[:, 3]
     transmitter = data[:, 4]
     mesh = trimesh.creation.icosphere(subdivisions=2, radius=5)
-    """vertex_indices_triangles = np.asarray(mesh.triangles)
-    #print(vertex_indices_triangles)
-
-    num_triangles = len(vertex_indices_triangles)
-    adjacency_matrix = np.zeros((num_triangles, num_triangles), dtype=int)
-
-    # Helper function to check if two triangles share a vertex
-    def share_vertex(triangle1, triangle2):
-        for vertex in triangle1:
-            if vertex in triangle2:
-                return True
-        return False
-
-    for i in range(num_triangles):
-        for j in range(i + 1, num_triangles):
-            if share_vertex(vertex_indices_triangles[i], vertex_indices_triangles[j]):
-                adjacency_matrix[i, j] = adjacency_matrix[j, i] = 1
-
-    num_triangles = len(mesh.faces)
-    adj_t = torch.tensor(adjacency_matrix)
-    edge_index = adj_t.nonzero().t().contiguous()
-    edge_index_str = str(edge_index.tolist())"""
     mesh = mesh.as_open3d
 
     vertices_array =np.asarray(mesh.vertices)
@@ -115,7 +265,6 @@ for i in range(len(results_list)):
         x1, y1, z1 = vertex
         vertice = Vertex(x1, y1, z1)
         vertices.append(vertice)
-    
     vertex_indexes_triangle = np.asarray(mesh.triangles)
     triangles = []
     for vertex_indexes in vertex_indexes_triangle:
@@ -160,37 +309,14 @@ for i in range(len(results_list)):
         # Assign the closest triangle as the value for the data point key
         triangle_dict[closest_triangle].append(data_point)
     
-    i = 0
     for triangle, data_points in triangle_dict.items():
         node = []
-        first_to_all_ratio = 0
-        if i == 0:
-            i += 1
+
         v1 = triangle.v1
         v2 = triangle.v2
         v3 = triangle.v3
         mass_center = [(v1.x + v2.x + v3.x)/3, (v1.y + v2.y + v3.y)/3, (v1.z + v2.z + v3.z)/3]
         polar_r, polar_theta, polar_phi = cart2sph(mass_center[0], mass_center[1], mass_center[2])
-        n_molecules = len(data_points)
-        t_max = 0
-        t_min = np.inf
-        all_times = []
-        f_transmitter_points = 0
-
-        for data_point in data_points:
-            time = data_point.time
-            if time > t_max:
-                t_max = time
-            if time < t_min:
-                t_min = time
-            all_times.append(time)
-            if data_point.t_id == 0:
-                f_transmitter_points += 1
-        #first_to_all_ratio = f_transmitter_points/(len(data_points)*1.0)
-        
-        t_avg = np.mean(all_times)
-        t_var = np.var(all_times)
-        t_std = np.std(all_times)
 
         area = areatriangle3d(v1.x, v1.y, v1.z,
                                 v2.x, v2.y, v2.z,
@@ -202,23 +328,43 @@ for i in range(len(results_list)):
         normal = np.cross(p2-p1, p3-p1)
         normal = normal / normal.sum()
 
-        if t_min == np.inf:
-            t_min = 0
-            t_avg = 0
-            t_var = 0
-            t_std = 0
+        n_hit_1, t_max_1, t_min_1, t_avg_1, t_var_1, t_std_1, x_avg_1, y_avg_1, z_avg_1 = first_time_slot(data_points)
+        n_hit_2, t_max_2, t_min_2, t_avg_2, t_var_2, t_std_2, x_avg_2, y_avg_2, z_avg_2 = second_time_slot(data_points)
+        n_hit_3, t_max_3, t_min_3, t_avg_3, t_var_3, t_std_3, x_avg_3, y_avg_3, z_avg_3 = third_time_slot(data_points)
+
         node.append(mass_center[0])
         node.append(mass_center[1])
         node.append(mass_center[2])
         node.append(polar_r)
         node.append(polar_theta)
         node.append(polar_phi)
-        node.append(n_molecules) #gaussian normalization
-        node.append(t_max) #t'ler de gaus norm olabilir
-        node.append(t_min)
-        node.append(t_avg)
-        node.append(t_var)
-        node.append(t_std)
+        node.append(n_hit_1) #gaussian normalization
+        node.append(t_max_1) #t'ler de gaus norm olabilir
+        node.append(t_min_1)
+        node.append(t_avg_1)
+        node.append(t_var_1)
+        node.append(t_std_1)
+        node.append(x_avg_1)
+        node.append(y_avg_1)
+        node.append(z_avg_1)
+        node.append(n_hit_2) #gaussian normalization
+        node.append(t_max_2) #t'ler de gaus norm olabilir
+        node.append(t_min_2)
+        node.append(t_avg_2)
+        node.append(t_var_2)
+        node.append(t_std_2)
+        node.append(x_avg_2)
+        node.append(y_avg_2)
+        node.append(z_avg_2)
+        node.append(n_hit_3) #gaussian normalization
+        node.append(t_max_3) #t'ler de gaus norm olabilir
+        node.append(t_min_3)
+        node.append(t_avg_3)
+        node.append(t_var_3)
+        node.append(t_std_3)
+        node.append(x_avg_3)
+        node.append(y_avg_3)
+        node.append(z_avg_3)
         node.append(area) #gaussian normalization(kesin)
         node.append(normal[0])
         node.append(normal[1])
@@ -233,13 +379,12 @@ for i in range(len(results_list)):
             first_to_all = count_zeros / (count_zeros + count_ones)
             second_to_all = count_ones / (count_zeros + count_ones)
         node.append(first_to_all)
-        #node.append(second_to_all)
+        node.append(second_to_all)
         
         nodes.append(node)
-        if len(data_points) == 0:
-            count += 1
     
-    np.savetxt('gnn_data_one_label/node_features_{}.txt'.format(exp_number), nodes, delimiter=", ", fmt='%1.5f')
+    nodes = normalize_area(nodes)
+    np.savetxt('gnn_data_son/node_features_{}.txt'.format(exp_number), nodes, delimiter=", ", fmt='%1.5f')
     #with open('gnn_data/edge_index_{}.txt'.format(exp_number), 'w+') as file2:
         #file2.write(edge_index_str)
 # k = 0
@@ -255,4 +400,3 @@ for i in range(len(results_list)):
 
 for i in range(100,110):
     print(nodes[i])
-print(count)
