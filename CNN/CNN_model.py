127dc5c466259906676515cef0e4ea990d311fc5
oykuselin <oykuselintoprak@windowslive.com>
2023-08-11 13:51:45 +0900

    CNN updates

--- CNN/CNN_model.py
+++ CNN/CNN_model.py
@@ -9,10 +9,12 @@ from sklearn.model_selection import train_test_split
 # Load and preprocess data
 data = np.load("CNN/Data.npy")
 inputs = data[:, :, :, :-2] # Reshape inputs to (num_samples, channels, height, width)
-labels = data[:, :, :, -2:] # Extract labels
+reshaped_inputs = np.transpose(inputs, (0, 3, 2, 1))
+labels = data[:, :, :, -1:] # Extract labels
+reshaped_labels = np.transpose(labels, (0, 3, 2, 1))
 
 # Split data into train and test sets
-train_inputs, test_inputs, train_labels, test_labels = train_test_split(inputs, labels, test_size=0.2, random_state=42)
+train_inputs, test_inputs, train_labels, test_labels = train_test_split(reshaped_inputs, reshaped_labels, test_size=0.2, random_state=42)
 
 # Convert the data to PyTorch tensors
 train_inputs = torch.Tensor(train_inputs)
@@ -32,33 +34,35 @@ class RegressionCNN(nn.Module):
         super(RegressionCNN, self).__init__()
     
         self.conv_layers = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(148, 200, kernel_size=3, padding=1),
+            nn.BatchNorm2d(200),
             nn.ReLU(inplace=True),
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(200, 320, kernel_size=3, padding=1),
+            nn.BatchNorm2d(320),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
+            nn.Conv2d(320, 520, kernel_size=3, padding=1),
+            nn.BatchNorm2d(520),
             nn.ReLU(inplace=True),
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
+            nn.Conv2d(520, 640, kernel_size=3, padding=1),
+            nn.BatchNorm2d(640),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
     
         self.linear_layers = nn.Sequential(
-            nn.Linear(256 * 80 * 37, 320),  # Adjust the input size to match the flattened shape
-            nn.Linear(320, 320 * 2),      # Adjust the output size to match the desired (320, 2) shape
+            nn.Linear(500 * 160 * 37, 520),  # Adjust the input size to match the flattened shape
+            nn.Linear(520, 320 * 2),      # Adjust the output size to match the desired (320, 2) shape
+            nn.Linear(640, 320),
+            nn.Sigmoid()
         )
     
     def forward(self, x):
         x = self.conv_layers(x)
         x = x.view(x.size(0), -1)         # Flatten the features
         x = self.linear_layers(x)
-        x = x.view(x.size(0), 1, 320, 2)  # Reshape to the desired output shape
+        x = x.view(x.size(0), 1, 320, 1)  # Reshape to the desired output shape
         return x
 
 # Create an instance of the model
---
+++ CNN/deneme_linear.py
@@ -0,0 +1,89 @@
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+# Load data
+data = np.load("CNN/Data_3label.npy")
+inputs = data[:, :, :, :-3]
+labels = data[:, :, :, -3:]
+
+X = np.array(inputs)
+y = np.array(labels)
+print(X.shape)
+print(y.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Reshape data
+X_train = X_train.reshape(X_train.shape[0], 320, 1, 148)
+X_test = X_test.reshape(X_test.shape[0], 320, 1, 148)
+y_train = y_train.reshape(y_train.shape[0], 1, 320, 3)  # Correct the shape here
+y_test = y_test.reshape(y_test.shape[0], 1, 320, 3)      # Correct the shape here
+
+
+class RegressionCNN(tf.keras.Model):
+    def __init__(self):
+        super(RegressionCNN, self).__init__()
+        
+        self.conv_layers = tf.keras.Sequential([
+            tf.keras.layers.Conv2D(32, kernel_size=(3, 1), padding='same', input_shape=(320, 1, 148)),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.ReLU(),
+            tf.keras.layers.Conv2D(64, kernel_size=(3, 1), padding='same'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.ReLU(),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 1)),
+            
+            tf.keras.layers.Conv2D(128, kernel_size=(3, 1), padding='same'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.ReLU(),
+            tf.keras.layers.Conv2D(256, kernel_size=(3, 1), padding='same'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.ReLU(),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 1))
+        ])
+        
+        self.flatten = tf.keras.layers.Flatten()
+        
+        self.linear_layers = tf.keras.Sequential([
+            tf.keras.layers.Dense(320, activation='linear'),  # Linear activation
+            tf.keras.layers.Dense(320 * 3, activation='linear')  # Linear activation
+        ])
+    
+    def call(self, x):
+        x = self.conv_layers(x)
+        x = self.flatten(x)
+        x = self.linear_layers(x)
+        x = tf.reshape(x, (-1, 1, 320, 3))
+        return x
+
+# Create an instance of the model
+model = RegressionCNN()
+
+# Compile the model
+model.compile(loss='mse', optimizer='adam')
+
+
+# Compile the model
+model.compile(loss='mse', optimizer='adam')
+
+# Train the model
+model.fit(X_train, y_train, epochs=10, batch_size=16)
+
+# Evaluate the model
+loss = model.evaluate(X_test, y_test)
+print(f"Test loss: {loss}")
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Print ground truth and prediction results for the first sample
+sample_index = 0 
+ground_truth = y_test[sample_index, 0, :]
+prediction = y_pred[sample_index, 0, :]
+
+print("Ground Truth:")
+print(ground_truth)
+
+print("Prediction:")
+print(prediction)
