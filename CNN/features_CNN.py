315b89ffe4dbff68713ad88b093803f1012c99d8
oykuselin <oykuselintoprak@windowslive.com>
2024-02-23 17:14:15 +0300

    data with neighbors added

--- CNN/features_CNN.py
+++ CNN/features_CNN.py
@@ -5,7 +5,7 @@ import re
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import trimesh
-import open3d as o3d
+#import open3d as o3d
 import torch
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
 
@@ -17,17 +17,17 @@ def find_ones_indexes(matrix, index):
     return ones_indexes
 
 current_directory = os.getcwd()
-sub_directory = './gnn_data_son'
-gnn_data_dir = os.path.join(current_directory, sub_directory)
+#sub_directory = './cnn_data_new_withoutneig'
+gnn_data_dir = "/home/oyku/yonsei/new_transmitter_localization/transmitter_localization/cnn_data_new/cnn_data_new_withoutneig"
 list_of_node_features = os.listdir(gnn_data_dir)
 
-if not os.path.exists("cnn_data_final"):
-    os.makedirs("cnn_data_final")
+if not os.path.exists("cnn_data_new_neighbors"):
+    os.makedirs("cnn_data_new_neighbors")
 
-adjacency_matrix = np.loadtxt(os.path.join(current_directory, './adjacency_matrix.txt'), delimiter=" ")
+adjacency_matrix = np.loadtxt("/home/oyku/yonsei/new_transmitter_localization/transmitter_localization/adjacency_matrix.txt", delimiter=" ")
 
 for i in range(len(list_of_node_features)):
-    features = np.loadtxt(os.path.join(gnn_data_dir, list_of_node_features[i]), delimiter=', ')
+    features = np.loadtxt(os.path.join(gnn_data_dir, list_of_node_features[i]), delimiter=',')
     if features.shape[0] != 320:
         continue
     
@@ -49,9 +49,9 @@ for i in range(len(list_of_node_features)):
                 features_row_to_add.append(features[neighbour][num])
         features_to_add.append(list(features_row_to_add))
 
-    labels_of_base = features[:, -2:]
-    features_without_labels = features[:, :-2]
-    cnn_features = np.hstack((features_without_labels, np.asarray(features_to_add)))
-    cnn_data_with_labels = np.hstack((cnn_features, labels_of_base))
-    print(cnn_data_with_labels.shape)
-    np.savetxt('cnn_data_final/node_features_{}.txt'.format(exp_number), cnn_data_with_labels, delimiter=", ", fmt='%1.5f')
+    #labels_of_base = features[:, -2:]
+    #features_without_labels = features[:, :-2]
+    cnn_features = np.hstack((features, np.asarray(features_to_add)))
+    #cnn_data_with_labels = np.hstack((cnn_features, labels_of_base))
+    #print(cnn_data_with_labels.shape)
+    np.savetxt('cnn_data_new_neighbors/node_features_{}.txt'.format(exp_number), cnn_features, delimiter=", ", fmt='%1.5f')
